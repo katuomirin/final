138857dfb58de5739c99d27117c2622ea43dce64
katuomirin <2003euph@gmail.com>
2024-01-12 11:48:46 +0900

    Update index.php

--- index.php
+++ index.php
@@ -2,22 +2,26 @@
 require 'dbconect.php';
 
 try {
-    $tableName = 'Job hunting';
-
-    $query = "SELECT * FROM `$tableName`";
+    $tableName = 'Job_hunting'; 
 
     $stmt = $pdo->query($query);
 
     $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
+    echo "<table border='1'>";
+    echo "<tr><th>ID</th><th>企業名</th><th>OtherColumn</th></tr>"; 
     foreach ($result as $row) {
-        print_r($row);
-        echo "<br>";
+        echo "<tr>";
+        foreach ($row as $value) {
+            echo "<td>$value</td>";
+        }
+        echo "</tr>";
     }
+
+    echo "</table>";
 } catch (PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
 }
 
-$conn = null;
-?>
+$pdo = null; 
 
